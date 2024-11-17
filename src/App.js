47007450945c4642f8e91bedc9edb23142709ab0
mamentudicky <dickymamentu@gmail.com>
2024-11-18 07:59:32 +0800

    import component

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import  "./App.css";
+import Navbar from  "./components/Navbar";
+import Header from  "./components/Header";
+import About from  "./components/About";
+import Qualification from  "./components/Qualification";
+import Contact from  "./components/Contact";
+import Footer from  "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <Navbar/>
+        <Header/>
+        <About/>
+        <Qualification/>
+        <Contact />
+        <Footer />
+        <i className="fa fa-2x fa-angle-down text-white scroll-to-bottom" />
+        <a href="#" className="btn btn-outline-dark px-0 back-to-top">
+          <i className="fa fa-angle-double-up" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
