81513f59d3281016b487641c7c7bccd3f87d9070
Nain09 <s22210396@student.unklab.ac.id>
2024-11-20 01:45:19 +0800

    menghubungkan ke firebase

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDO5fgqeXap4gx9WRSGvnw-YQJuRTzbkHs",
-  authDomain: "myonlycv-51125.firebaseapp.com",
-  projectId: "myonlycv-51125",
-  storageBucket: "myonlycv-51125.firebasestorage.app",
-  messagingSenderId: "197713532661",
-  appId: "1:197713532661:web:289d926627d213259297bc",
-  measurementId: "G-GE3GHTQ055"
+  apiKey: "AIzaSyC29KQO7RO4PRtN0Btsxanjv0UTlWjMQ7g",
+  authDomain: "kingcv-32608.firebaseapp.com",
+  projectId: "kingcv-32608",
+  storageBucket: "kingcv-32608.firebasestorage.app",
+  messagingSenderId: "751253806114",
+  appId: "1:751253806114:web:ef26348b9a0611e2cc3e7b",
+  measurementId: "G-GJCJEP30SE"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
